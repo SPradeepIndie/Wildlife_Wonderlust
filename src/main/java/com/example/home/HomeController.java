87f9f4e6a07e5ed5201ce7e4e92eb41631d9c893
Sanjaya Pradeep <katuwawala12365@usci.ruh.ac.lk>
2024-01-18 17:26:14 +0530

    home page is done

--- src/main/java/com/example/home/HomeController.java
+++ src/main/java/com/example/home/HomeController.java
@@ -6,58 +6,69 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.PrivateKey;
 
 public class HomeController {
 
+    protected String n="Location1_details.fxml";
     @FXML
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    //get reference to the location name and see more button;To appear when triggerSeeMore btn clicked
     @FXML
-    private Label welcomeText;
+    private Label loc_name;
+    @FXML
+    private Button seeMore_btn;
 
-
-    public void gotolocation1(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("location1.fxml"));
+    public void seemore(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(n));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
-    public void gotolocation2(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("location2.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    
+    //Methods to handle all location trigger button
+    @FXML
+    private void handleLoc1ButtonClick(ActionEvent event) {
+        loc_name.setText("Location1");
+        seeMore_btn.setOpacity(1);
+        seeMore_btn.setText("See more");
+        n="Location1_details.fxml";
     }
-
-    public void gotolocation3(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("location3.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private void handleLoc2ButtonClick(ActionEvent event) {
+        loc_name.setText("Location2");
+        seeMore_btn.setOpacity(1);
+        seeMore_btn.setText("See more");
+        n="Location2_details.fxml";
     }
-
-    public void gotolocation4(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("location4.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private void handleLoc3ButtonClick(ActionEvent event) {
+        loc_name.setText("Location3");
+        seeMore_btn.setOpacity(1);
+        seeMore_btn.setText("See more");
+        n="Location3_details.fxml";
     }
-
-    public void gotolocation5(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("location5.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private void handleLoc4ButtonClick(ActionEvent event) {
+        loc_name.setText("Location4");
+        seeMore_btn.setOpacity(1);
+        seeMore_btn.setText("See more");
+        n="Location4_details.fxml";
+    }
+    @FXML
+    private void handleLoc5ButtonClick(ActionEvent event) {
+        loc_name.setText("Location5");
+        seeMore_btn.setOpacity(1);
+        seeMore_btn.setText("See more");
+        n="Location5_details.fxml";
     }
 }
--- src/main/java/com/example/home/Test.java
+++ src/main/java/com/example/home/Test.java
@@ -3,22 +3,27 @@ package com.example.home;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Test extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(Test.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
-        stage.setTitle("Welcome!");
+        scene.getStylesheets().add(getClass().getResource("/home.css").toExternalForm());//Insert the home.css stylesheet
+
+        //Image icon=new Image('logo.png'); //set the stage title icon
+        //stage.getIcons().add(icon);
+        stage.setTitle("Wildlife Wonderlust!");//set the stage title icon
         stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setResizable(false);//close resize option
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
